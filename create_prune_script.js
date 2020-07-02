c852281b5b1d7a30531ebf80ca514310d6917aa3
Bishaka Samuel <sbishaka@fenixintl.com>
2020-07-02 14:27:48 +0300

    make init commit of prune-duplicates

---
+++ create_prune_script.js
@@ -0,0 +1,46 @@
+
+const getTemplate = (file) => new Promise ( (resolve, reject) => {
+    const fs = require('fs');
+    fs.readFile(file, 'utf8', (err,data) => {
+        if(err){
+            return reject(err);
+        }
+        return resolve(data);
+    });
+})
+
+const main = async () => {
+
+    const file = './template.txt';
+    const templateStr = await getTemplate(file);
+
+    const Handlebars = require("handlebars");
+    
+    Handlebars.registerHelper('join', function (array) {        
+        return array.join(", ")
+    })
+
+    Handlebars.registerHelper('comma', function (array) {
+        return (array.length > 0)?",":""
+    })    
+
+    Handlebars.registerHelper('isLast', function (value,array) {
+        return value == array[array.length - 1];
+    });
+      
+
+    const template = Handlebars.compile(templateStr);
+    
+    const input = {
+        id:"ctid",
+        span:[
+            "call_id",
+            "ticket_id"
+        ],
+        table:"public.ticketcall"
+    }
+    const output = template(input);
+    console.log(output);
+}
+
+main();
